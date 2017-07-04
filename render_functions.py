68ba84edab80ebd803e1c8b9cf55fa0973b7aece
Tyler Standridge <tstand90@gmail.com>
2017-07-04 08:54:27 -0700

    Simplifying the loop in render_all

--- render_functions.py
+++ render_functions.py
@@ -1,13 +1,12 @@
 def render_all(con, entities, game_map, root_console, screen_width, screen_height, colors):
     # Draw all the tiles in the game map
-    for y in range(game_map.height):
-        for x in range(game_map.width):
-            wall = not game_map.transparent[x, y]
+    for x, y in game_map:
+        wall = not game_map.transparent[x, y]
 
-            if wall:
-                con.draw_char(x, y, None, fg=None, bg=colors.get('dark_wall'))
-            else:
-                con.draw_char(x, y, None, fg=None, bg=colors.get('dark_ground'))
+        if wall:
+            con.draw_char(x, y, None, fg=None, bg=colors.get('dark_wall'))
+        else:
+            con.draw_char(x, y, None, fg=None, bg=colors.get('dark_ground'))
 
     # Draw all entities in the list
     for entity in entities:
